edbd91f56218222f80507ee1e8248cb85bfd408f
Erwan Mawart <bluewancontact@gmail.com>
2024-11-11 19:17:48 -0500

    Update transfert function's schematic & find Kp equation depending on Kh

--- Devoir_2/answer_files/calcRouthHurwitz.m
+++ Devoir_2/answer_files/calcRouthHurwitz.m
@@ -1,10 +1,10 @@
 system_config
-clear Kp
+clear Kp Kh
 
 sys = Calc_Sys();
 
-syms Kp s
-TF = subs(sys, [sym('MR') sym('mR') sym('Kb') sym('Cb') sym('CR') sym('T') sym('m') sym('c') sym('Kh')], [MR mR Kb Cb CR T m c Kh]);
+syms Kp Kh s
+TF = subs(sys, [sym('MR') sym('mR') sym('Kb') sym('Cb') sym('CR') sym('T') sym('m') sym('c')], [MR mR Kb Cb CR T m c]);
 [Num, Den] = numden(TF)
 [an, terms] = coeffs(Den, s);
 l_terms = length(terms);
@@ -30,22 +30,21 @@ Kp_range = struct('max', -Inf, 'min', 0);
 assume(Kp > 0)          % car s0 = Kp*55000 = 0
 for i = 1:l_terms
     if ~isempty(find(symvar(s(i, 1)) == Kp)) || ~isempty(find(symvar(s(i, 1)) == Kh))
-        sol = solve(s(i, 1) == 0, Kp);
+        sol = simplify(solve(s(i, 1) == 0, Kp));
 
-        Kp_sol = double(sol);
+        Kp_sol = double(subs(sol, Kh, Kh_init));
         if max(Kp_sol) > Kp_range.max
             Kp_range.max = max(Kp_sol);
+            Kpmax_formula = sol(find(Kp_sol == max(Kp_sol)));
         end
     end
 end
 
+disp("On détermine grâce à la formule : " + string(Kpmax_formula))
 disp("<strong>" + Kp_range.min + " < Kp < " + Kp_range.max + "</strong>")
 
-disp(subs(s, Kp, Kp_range.max))
 
-%hold on;
-TF_max = subs(TF, Kp, Kp_range.max);
+TF_max = subs(TF, [Kp Kh], [Kp_range.max Kh_init]);
 TFFun_max = matlabFunction(TF_max);
 TFFun_max = str2func(regexprep(func2str(TFFun_max), '\.([/^\\*])', '$1'));
 rlocus(tf(TFFun_max(tf('s'))))
-%hold off;
