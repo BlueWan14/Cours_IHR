1053a4d96b60451169362810d0b645d2b5772b2c
Erwan Mawart <bluewancontact@gmail.com>
2024-11-13 19:17:24 -0500

    Complete question 1.3 ... but it's probably false

--- Devoir_2/answer_files/calcRouthHurwitz.m
+++ Devoir_2/answer_files/calcRouthHurwitz.m
@@ -6,31 +6,38 @@ clear Kp Kh
 
 syms Kp Kh s
 disp("<strong>Boucle de vitesse</strong>")
-BoucleVitesse = subs(BoucleVitesse, [sym('MR') sym('mR') sym('Kb') sym('Cb') sym('CR') sym('T')], [MR mR Kb Cb CR T]);
-S_vitesse = calcTabRH(BoucleVitesse, s);
+S_vitesse = calcTabRH(BoucleVitesse, [sym('MR') sym('mR') sym('Kb') sym('Cb') sym('CR') sym('T')], [MR mR Kb Cb CR T], s);
+
 disp("<strong>Boucle complete</strong>")
-Boucle = subs(Boucle, [sym('MR') sym('mR') sym('Kb') sym('Cb') sym('CR') sym('T') sym('m') sym('c')], [MR mR Kb Cb CR T m c]);
-S_boucle = calcTabRH(Boucle, s);
+%Boucle = subs(Boucle, [sym('MR') sym('mR') sym('Kb') sym('Cb') sym('CR') sym('T') sym('m') sym('c')], [MR mR Kb Cb CR T m c]);
+S_boucle = calcTabRH(Boucle, [sym('MR') sym('mR') sym('Kb') sym('Cb') sym('CR') sym('T') sym('m') sym('c')], [MR mR Kb Cb CR T m c], s);
 
-eqs = [S_vitesse(symvar(S_vitesse(:, 1)) == Kp, 1), S_boucle(find(symvar(S_boucle(:, 1)) == Kp) || find(symvar(S_boucle(:, 1)) == Kh), 1)];
-assume(Kh > 0)      % car la rigidité de l'humain ne peux pas être négative
-cond = solve(eqs > 0, Kp, ReturnConditions=true);
-disp("<strong>" + string(subs(cond.conditions, sym('x'), Kp)) + "</strong>")
+disp("<strong>Kp = -100 : </strong>")
+disp(simplify(subs(S_boucle, Kp, -100)))
+disp("<strong>Kp = 454.93 : </strong>")
+disp(simplify(subs(S_boucle, Kp, 454.93)))
+
+%eqs = [S_vitesse(symvar(S_vitesse(:, 1)) == Kp, 1), S_boucle(find(symvar(S_boucle(:, 1)) == Kp) || find(symvar(S_boucle(:, 1)) == Kh), 1)];
+%assume(Kh > 0)      % car la rigidité de l'humain ne peux pas être négative
+%assume(Kp == -100)
+%cond = solve(S_boucle(3, 1) > 0, Kh, ReturnConditions=true);
+%disp(cond.Kh)
+%disp("<strong>" + string(subs(cond.conditions, sym('x'), Kp)) + "</strong>")
 
 
-K_test = [1e-6 1e-6];
+%K_test = [1e-6 1e-6];
 
 %disp(subs(S_boucle, [Kh Kp], K_test))
 
-TF = subs(Boucle, [Kh Kp], K_test);
-TFFun = matlabFunction(TF);
-TFFun = str2func(regexprep(func2str(TFFun), '\.([/^\\*])', '$1'));
-figure;
-rlocus(tf(TFFun(tf('s'))))
+%TF = subs(Boucle, [Kh Kp], K_test);
+%TFFun = matlabFunction(TF);
+%TFFun = str2func(regexprep(func2str(TFFun), '\.([/^\\*])', '$1'));
+%figure;
+%rlocus(tf(TFFun(tf('s'))))
 
 
 
-function S = calcTabRH(TF, s)
+function S = calcTabRH(TF, Oldvars,  Newvars, s)
     [~, Den] = numden(TF);
     [an, terms] = coeffs(Den, s);
     l_terms = length(terms);
@@ -49,6 +56,8 @@ function S = calcTabRH(TF, s)
             S(k, n) = (S(k+1, 1)*S(k+2, n+1)-S(k+2, 1)*S(k+1, n+1))/S(k+1, 1);
         end
     end
+    S = simplify(subs(S, Oldvars, Newvars));
+
     disp("Tableau du critère de Routh-Hurwitz :")
     disp(S)
 end
